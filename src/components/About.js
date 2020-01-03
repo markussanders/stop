efcf1894315cb9d74fa4353cc1a418edac45f57d
xmksz3 <markus.sanderst@gmail.com>
2020-01-03 16:09:56 -0600

    fixed verbage to enhance clarity

--- src/components/About.js
+++ src/components/About.js
@@ -13,10 +13,10 @@ const About = props => {
                     STOP was inspired by a game in a Chinatown cafe in which you can win a free boba/milk tea if you
                     score `10.000` seconds on a blind timer. What makes this seemingly-simple task so difficult is 
                     the struggle of internalizing steady rhythmn. In order to get better at this, I created four modes:
-                    "easy" –– where audio cues are played to signify each second, "medium" –– in which cues are only given
-                    at the start of the timer, "hard" –– where no cues are played and "impossible" –– similar to "hard" in 
-                    which there are no audio cues provided, however, unlike the previous three modes in which achieving `10`
-                    seconds suffices a 'win', users must achieve a perfect `10.000`.
+                    "easy" –– where visual cues are displayed to signify each second, "medium" –– in which cues are only
+                    given at the start of the timer, "hard" –– where no cues are shown and "impossible" –– similar to "hard"
+                    in which there are no audio cues provided, however, unlike the previous three modes in which achieving
+                    ten whole-seconds suffices a 'win', users must achieve a perfect "10.000".
                 </p>
             <button className="modal-close is-large" aria-label="close" onClick={() => props.toggleAbout()}></button>
             </div>
